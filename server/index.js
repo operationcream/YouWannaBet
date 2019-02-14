95733bb50bd4581fbc484ab8eeaf217f6741cda0
lauraannpena <lauraannpena@gmail.com>
2019-02-14 14:13:20 -0600

    added basic index.js for server

--- server/index.js
+++ server/index.js
@@ -4,4 +4,4 @@ const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`listening on port ${port}!`));
+app.listen(port, () => console.log(`listening on port ${port}!`))
