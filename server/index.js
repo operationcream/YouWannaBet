1ec2b57c5b4a84b060b8638bbd59f1800305c96c
lauraannpena <lauraannpena@gmail.com>
2019-02-19 07:58:00 -0600

    added feathers to server files

--- server/index.js
+++ server/index.js
@@ -1,7 +1,34 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000
+// https://docs.feathersjs.com/api/express.html
+// check out docs for more info
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const feathers = require('@feathersjs/feathers');
+const express = require('@feathersjs/express');
 
-app.listen(port, () => console.log(`listening on port ${port}!`))
+const app = express(feathers());
+const port = process.env.PORT || 3000;
+
+// Register a service
+app.use('/todos', {
+  get(id) {
+    return Promise.resolve({ id });
+  },
+});
+
+// Register an Express middleware
+app.use('/test', (req, res) => {
+  res.json({
+    message: 'Hello world from Express middleware',
+  });
+});
+
+// Register multiple Express middleware functions
+app.use('/test', (req, res, next) => {
+  res.data = 'Step 1 worked';
+  next();
+}, (req, res) => {
+  res.json({
+    message: `Hello world from Express middleware ${res.data}`,
+  });
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
