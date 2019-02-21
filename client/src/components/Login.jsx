68cd758313b6e029cbc0ef263126261542ae2e3e
David <dlum988@gmail.com>
2019-02-21 11:17:31 -0600

    created Login component and rendering all components to index

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login Area</h1>
+    </div>
+  );
+};
+
+export default Login;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,12 +4,15 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 import List from './components/List.jsx';
 import Search from './components/searchGames.jsx';
+import Login from './components/Login.jsx';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       items: [],
+      loggedIn: false,
     };
     this.getItems = this.getItems.bind(this);
   }
@@ -34,6 +37,10 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <div>
+          <h1>Login</h1>
+          <Login />
+        </div>
         <div className="col-md-6 offset-md-3">
           <h1>Item List</h1>
           <List items={this.state.items} />
@@ -41,7 +48,7 @@ class App extends React.Component {
         {/* Places the two search bars.  getGames HELPER FUNCTION not yet built */}
         <div className="col-md-6 offset-md-3">
           <Search
-            handleSearchInputChange={this.getGames.bind(this)}
+            // handleSearchInputChange={this.getGames.bind(this)}
           />
         </div>
       </div>
