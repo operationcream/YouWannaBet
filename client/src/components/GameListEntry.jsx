d297c1f8c96b4f360fd03aa33caf12e3e8bcff85
voodoochum <brian@aquavisionnola.com>
2019-02-25 10:49:30 -0600

    List Entries Created

---
+++ client/src/components/GameListEntry.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+
+const GameListEntry = ({ game, onClick }) => (
+  <div className="game-list-entry">
+    <div className="game-body">
+      <div className="game-list-detail" onClick={() => {onClick(game)}}><h2>{game.Date} {game.homeTeam} VS {game.awayTeam} </h2></div>
+    </div>
+  </div>
+);
+
+export default GameListEntry;
