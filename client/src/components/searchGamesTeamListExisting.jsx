9e6114af05ccd94d1586c3cedd3a2265b8febfac
voodoochum <brian@aquavisionnola.com>
2019-02-24 21:57:29 -0600

    New Date Formatted properly for comparison

--- client/src/components/searchGamesTeamListExisting.jsx
+++ client/src/components/searchGamesTeamListExisting.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 // container to hold users live bets
 const SearchGamesList = (props) => {
 console.log(props);
+  let games = props.games;
+  let today = new Date().toISOString().slice(0, 10).split('-');
+  today = today.join('');
+  console.log(today);
+
   return (
     <div className="teamBetList">
       <h3>List of Bets</h3>
