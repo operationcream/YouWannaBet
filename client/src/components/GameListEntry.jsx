095415c39384d78bfa6ea977ffe35c4f44185c82
voodoochum <brian@aquavisionnola.com>
2019-02-25 16:03:39 -0600

    Images Loading

--- client/src/components/GameListEntry.jsx
+++ client/src/components/GameListEntry.jsx
@@ -1,28 +1,28 @@
-var React = require('react');
+let React = require('react');
 
 
 const GameListEntry = ({ game, onClick }) => (
   <div className="game-list-entry">
     <div className="game-body">
-        <div className="game-list-detail" key={game.homeTeam} onClick={() => { onClick(game) }}>
-      <table is='table'>
-      <tbody>
+      <div className="game-list-detail" key={game.homeTeamImage} onClick={() => { onClick(game); }}>
+        <table is="table">
+        <tbody>
         <tr id="row">
-          <td id='cell0'>
-            <img src={game.homeTeam} />
+          <td id="cell0">
+            <img src={game.homeTeamImage} />
           </td>
-          <td id='cell5'> </td>
-          <td id='cell1'>
-                <h1>VS</h1>
+          <td id="cell5" />
+          <td id="cell1">
+            <h1>VS</h1>
           </td>
-              <td id='cell6'> </td>
-          <td id='cell2'>
-              <img src={game.awayTeam} />
-          </td> 
-              <td id='cell7'> </td>
-              <td id='cell3'>
-                <h2> on {game.date}</h2>
-              </td>       
+          <td id="cell6" />
+          <td id="cell2">
+            <img src={game.awayTeamImage} />
+          </td>
+          <td id="cell7" />
+          <td id="cell3">
+            <h2> on {game.date}</h2>
+          </td>
         </tr>
       </tbody>
       </table>
