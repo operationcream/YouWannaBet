b9a61772989a23a4e7c0d379bc7f647f07a78a3b
voodoochum <brian@aquavisionnola.com>
2019-02-24 21:01:13 -0600

    Adding a List Component

---
+++ client/src/components/searchGamesTeamListExisting.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// container to hold users live bets
+const SearchGamesList = ({bets}) => (
+
+  <div className="teamBetList">
+    <h3>List of Bets</h3>
+  </div>
+);
+
+export default SearchGamesList;
