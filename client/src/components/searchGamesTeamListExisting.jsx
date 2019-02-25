fcb73a6c6110ba76fa7f57ca15b4245f4c21006a
voodoochum <brian@aquavisionnola.com>
2019-02-24 22:49:47 -0600

    Properly Formatted Games Present at the Games List Component.  Ready to be sent to the List Item that needs to be created

--- client/src/components/searchGamesTeamListExisting.jsx
+++ client/src/components/searchGamesTeamListExisting.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 // container to hold users live bets
 const SearchGamesList = (props) => {
 console.log(props);
+  let teams = props.teams;
   let games = props.games;
   let today = new Date().toISOString().slice(0, 10).split('-');
   today = today.join('');
   // console.log(today);
-  let gamesAfterToday = [];
+  const gamesAfterToday = [];
   let count = 0;
   games.forEach((game) => {
     if (game.homeStartDate > today && count < 10) {
@@ -14,7 +15,22 @@ console.log(props);
       count += 1;
     }
   });
-console.log(gamesAfterToday);
+  const formattedGames = [];
+  gamesAfterToday.forEach((game) => {
+    const formattedGame = {};
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < teams.length; i++) {
+      if (game.hTeam.teamId === teams[i].teamId) {
+        formattedGame.HomeTeam = teams[i].fullName;
+      } else if (game.vTeam.teamId === teams[i].teamId) {
+        formattedGame.AwayTeam = teams[i].fullName;
+      }
+      formattedGame.Date = game.homeStartDate;
+    }
+    formattedGames.push(formattedGame);
+  });
+
+  console.log(formattedGames);
 
   return (
     <div className="teamBetList">
