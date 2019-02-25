d37d1aab99fba2b8061f5afaecbae285c67c39a7
lauraannpena <lauraannpena@gmail.com>
2019-02-25 16:14:05 -0600

    reverted back to older version of userbetlist

--- client/src/components/UserBetList.jsx
+++ client/src/components/UserBetList.jsx
@@ -1,30 +1,13 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import UserBetListItem from './UserBetListItem.jsx';
 
-const styles = theme => ({
-  root: {
-    ...theme.mixins.gutters(),
-    paddingTop: theme.spacing.unit * 2,
-    paddingBottom: theme.spacing.unit * 2,
-  },
-});
-
 // container to hold users live bets
-
-class UserBetList extends React.Component {
-
-  render() {
-    return (
-      <Paper className={this.props.classes.root} elevation={1}>
-        <Typography component="p">
-          {this.props.userBets.map(userBet => <UserBetListItem key={userBet.id} userBet={userBet} />)}
-        </Typography>
-      </Paper>
-    );
-  }
+function UserBetList({ userBets }) {
+  return (
+    <div className="userBetList">
+      {userBets.map(userBet => <UserBetListItem key={userBet.id} userBet={userBet} />)}
+    </div>
+  );
 }
 
-export default withStyles(styles)(UserBetList);
+export default UserBetList;
