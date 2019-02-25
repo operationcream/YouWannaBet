27fef3ffd878679ab661f27f0aac195922b8402d
voodoochum <brian@aquavisionnola.com>
2019-02-24 22:08:19 -0600

    Games Array contains the next 10 games after today's date

--- client/src/components/searchGamesTeamListExisting.jsx
+++ client/src/components/searchGamesTeamListExisting.jsx
@@ -5,7 +5,16 @@ console.log(props);
   let games = props.games;
   let today = new Date().toISOString().slice(0, 10).split('-');
   today = today.join('');
-  console.log(today);
+  // console.log(today);
+  let gamesAfterToday = [];
+  let count = 0;
+  games.forEach((game) => {
+    if (game.homeStartDate > today && count < 10) {
+      gamesAfterToday.push(game);
+      count += 1;
+    }
+  });
+console.log(gamesAfterToday);
 
   return (
     <div className="teamBetList">
