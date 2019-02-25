f5fe64b36cae541831abea70c9173071dd1f76f2
voodoochum <brian@aquavisionnola.com>
2019-02-24 21:35:08 -0600

    List Component recieving Teams and Games

--- client/src/components/searchGamesTeamListExisting.jsx
+++ client/src/components/searchGamesTeamListExisting.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 // container to hold users live bets
-const SearchGamesList = ({bets}) => (
-
-  <div className="teamBetList">
-    <h3>List of Bets</h3>
-  </div>
-);
+const SearchGamesList = (props) => {
+console.log(props);
+  return (
+    <div className="teamBetList">
+      <h3>List of Bets</h3>
+    </div>
+  );
+};
 
 export default SearchGamesList;
