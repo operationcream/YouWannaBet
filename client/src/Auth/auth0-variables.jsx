c21f62026023dd82cfb0b8e8cadba520bcd9b89c
dlum88 <dlum988@gmail.com>
2019-02-25 19:06:13 -0600

    done (#58)
    * setting things up for deployment
    
    * vscode
    
    * changed static serve
    
    * added white space
    
    * changed auth callbackurl
    
    * adding client/dist/
    
    * fixed auth0 variables
    
    * added callback to end of callbackurl
    
    * changed clientId in auth0
    
    * changed auth0 variables
    
    * fixing errors
    
    * fixing bugs
    
    * centered photos

--- client/src/Auth/auth0-variables.jsx
+++ client/src/Auth/auth0-variables.jsx
@@ -1,7 +1,8 @@
 const AUTH_CONFIG = {
   domain: 'youwannabet.auth0.com',
   clientId: '3yUuP72XyjtMvg7kMRpLVjAYfvjIHARe',
-  callbackUrl: 'http://localhost:3000/callback',
+  callbackUrl: 'https://polar-brushlands-88901.herokuapp.com/callback',
+  // callbackUrl: 'http://localhost:3000/callback',
 };
 
 export { AUTH_CONFIG as default };
