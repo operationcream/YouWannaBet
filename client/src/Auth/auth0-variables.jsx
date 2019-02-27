573744b669edf67c07a62fe14480a2d1fd8a076c
lauraannpena <lauraannpena@gmail.com>
2019-02-27 09:03:34 -0600

    changed auth0 callback url to deployed url

--- client/src/Auth/auth0-variables.jsx
+++ client/src/Auth/auth0-variables.jsx
@@ -1,7 +1,7 @@
 const AUTH_CONFIG = {
   domain: 'youwannabet.auth0.com',
   clientId: '3yUuP72XyjtMvg7kMRpLVjAYfvjIHARe',
-  callbackUrl: 'http://localhost:3000/callback',
+  callbackUrl: 'http://18.205.113.222/callback',
 };
 
 export { AUTH_CONFIG as default };
